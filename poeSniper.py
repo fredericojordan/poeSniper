808b2c695ddc91e80cd66e1c27edaa1971ec2dd7
Frederico Jordan <fredericojordan@gmail.com>
2017-03-13 14:49:22 -0300

    GET and parse json files

--- poeSniper.py
+++ poeSniper.py
@@ -1,5 +1,29 @@
 import urllib.request
+import json
 
-base_site = "http://api.pathofexile.com/public-stash-tabs"
+BASE_SITE = "http://api.pathofexile.com/public-stash-tabs"
 
-urllib.request.urlopen(base_site).read()
+def getStashes(id=""):
+    target = BASE_SITE
+    
+    if (id != ""):
+        target = target + "?id=" + str(id)
+        
+    page = str(urllib.request.urlopen(target).read())[2:-1]
+    page = page.replace("\\'", "'")
+    page = page.replace("\\\\", "\\")
+    return page
+
+dump_file = open('dump.txt', 'w')
+next_page = ""
+for x in range(10):
+    data = getStashes(next_page)
+    dump_file.write(data)
+    dump_file.write("\n")
+    jPage = json.loads(data)
+    next_page = jPage["next_change_id"]
+    print(next_page)
+    
+    
+dump_file.close()
+print("Done!")
