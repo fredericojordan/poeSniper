1fe5f47cff7b39a2cfa49c670ee82737a490b6fe
Frederico Jordan <fredericojordan@gmail.com>
2017-03-15 16:14:44 -0300

    Converting prices to chaos

--- poeSniper.py
+++ poeSniper.py
@@ -6,11 +6,12 @@ CURRENT_LEAGUE = "Legacy"
 API_BASE_URL = "http://api.pathofexile.com/public-stash-tabs"
 STARTING_PAGE = "49919976-52967025-49502222-57596848-53578103" # empty string for first page
 TOTAL_PAGES = 1
-MARKET_PRICES = {}
+MARKET_PRICES = [{} for _ in range(10)]
 
 UNIQUE_FLASK_PRICES_URL = "http://cdn.poe.ninja/api/Data/GetUniqueFlaskOverview"
 CARD_PRICES_URL = "http://api.poe.ninja/api/Data/GetDivinationCardsOverview"
 PROPHECY_PRICES_URL = "http://api.poe.ninja/api/Data/GetProphecyOverview"
+CURRENCY_PRICES_URL = "http://cdn.poe.ninja/api/Data/GetCurrencyOverview"
 
 class ITEM_TYPES:
     Normal, Magic, Rare, Unique, Gem, Currency, Card, Quest, Prophecy, Relic = range(10)
@@ -20,7 +21,12 @@ def getNinjaPrices(url):
     response = requests.get(url, params = params)
     prices = response.json().get('lines')
     return dict( [i.get('name'), float(i.get('chaosValue')) ] for i in prices )	
-    
+
+def getNinjaCurrency(url):
+    params = {'league': CURRENT_LEAGUE, 'time': time.strftime("%Y-%m-%d")}
+    response = requests.get(url, params = params)
+    prices = response.json().get('lines')
+    return dict( [i.get('currencyTypeName'), float(i.get('chaosEquivalent')) ] for i in prices )
 
 def getApiPage(page_id=""):
     target = API_BASE_URL
@@ -49,8 +55,22 @@ def isSelling(item):
 def isSellingBuyout(item):
     return "note" in item.keys() and item["note"].startswith("~b/o")
 
-def getItemSellingPrice(item):
+def getItemSellingOffer(item):
     return item["note"].split()
+
+def offer2chaos(offer):
+    if "chaos" in offer[2]:
+        return float(offer[1])
+    
+    if "fus" in offer[2]:
+        return float(offer[1])*MARKET_PRICES[ITEM_TYPES.Currency]["Orb of Fusing"]
+    
+    for k,v in MARKET_PRICES[ITEM_TYPES.Currency].items():
+        if offer[2].lower() in k.lower():
+            return float(float(offer[1])*v)
+
+def getItemSellingPrice(item):
+    return offer2chaos(getItemSellingOffer(item))
  
 def getItemName(item):
     return item["typeLine"]
@@ -74,13 +94,14 @@ def isEmpty(stash):
     return stash["items"] == []
 
 def getItemMarketPrice(item):
-    return MARKET_PRICES[getItemName(item)]
+    if getItemType(item) == ITEM_TYPES.Card:
+        return MARKET_PRICES[ITEM_TYPES.Card][getItemName(item)]
                 
 def getTradeMessage(stash, item):
     characterName = getCharacterName(stash)
     itemName = getItemName(item)
-    price = item["note"].split()[1]
-    currency = item["note"].split()[2]
+    price = getItemSellingOffer(item)[1]
+    currency = getItemSellingOffer(item)[2]
     league = getItemLeague(item)
     stashName = getStashName(stash)
     w = item.get('w')
@@ -92,73 +113,65 @@ def findDivDeals(stashes):
         items = s['items']
         
         for i in items:
-            if getItemLeague(i) == CURRENT_LEAGUE and getItemType(i) == ITEM_TYPES.Card:
-                price = i.get('note')
-                
-                # Gets items with price, in chaos, b/o only
-                if price and ('~b/o' and 'chaos' in price):
-                    
-                    price = float(price.split()[1])
-                    
-                    if (getItemMarketPrice(i) - price) > -5.0:
-                        print(getTradeMessage(s, i))
+            if getItemLeague(i) == CURRENT_LEAGUE and getItemType(i) == ITEM_TYPES.Card and isSellingBuyout(i):
+                profit = getItemMarketPrice(i) - getItemSellingPrice(i)
+                if profit > -5.0:
+                    print(getTradeMessage(s, i))
 
 
-
-
-MARKET_PRICES.update(getNinjaPrices(CARD_PRICES_URL))
-MARKET_PRICES.update(getNinjaPrices(PROPHECY_PRICES_URL))
-MARKET_PRICES.update(getNinjaPrices(UNIQUE_FLASK_PRICES_URL))
-
+MARKET_PRICES[ITEM_TYPES.Card].update(getNinjaPrices(CARD_PRICES_URL))
+MARKET_PRICES[ITEM_TYPES.Prophecy].update(getNinjaPrices(PROPHECY_PRICES_URL))
+MARKET_PRICES[ITEM_TYPES.Unique].update(getNinjaPrices(UNIQUE_FLASK_PRICES_URL))
+MARKET_PRICES[ITEM_TYPES.Currency].update(getNinjaCurrency(CURRENCY_PRICES_URL))
 
 # VSZ
 data = loadApiPageFromFile('response.txt')
 stashes = data['stashes']
 findDivDeals(stashes)
 
-
-# FVJ
-dump_file = open('dump.txt', 'w')
-next_page = STARTING_PAGE
-
-for p in range(TOTAL_PAGES):
-    print("---- Page {} ----".format(p+1))
-
-    #jPage = getApiPage(next_page)
-    jPage = loadApiPageFromFile('response.txt')
-    
-    stashes = jPage["stashes"]
-    stashes_count = len(stashes)
-    
-    empty_stashes_count = 0
-    total_items_count = 0
-    card_count = 0
-    
-    for s in range(stashes_count):
-        items = stashes[s]["items"]
-        if ( isEmpty(stashes[s]) ):
-            empty_stashes_count += 1
-        else:
-            items = stashes[s]["items"]
-            total_items_count += len(items)
-            for i in range(len(items)):
-                if (getItemType(items[i]) == ITEM_TYPES.Card and getItemLeague(items[i]) == CURRENT_LEAGUE):
-                    card_count += 1
-                    item_name = getItemName(items[i])
-                    
-                    if isSellingBuyout(items[i]):
-                        print("{}: {} ({} chaos)".format(item_name, ' '.join(getItemSellingPrice(items[i])[1:]), MARKET_PRICES[item_name]))                    
-                        player_info = stashes[s]
-                        player_info["items"] = []
-                        dump_file.write("{} {}\n".format(player_info, items[i]))
-    
-    print("----")
-    print("This page has {} items on {} stashes and {} empty stashes".format(total_items_count, stashes_count-empty_stashes_count, empty_stashes_count))
-    print("{} divination cards on legacy league".format(card_count))
-    
-    next_page = jPage["next_change_id"]
-    print("Next page: {}".format(next_page))
-
-dump_file.close()
+# # FVJ
+# dump_file = open('dump.txt', 'w')
+# next_page = STARTING_PAGE
+#  
+# for p in range(TOTAL_PAGES):
+#     print("---- Page {} ----".format(p+1))
+#  
+#     #jPage = getApiPage(next_page)
+#     jPage = loadApiPageFromFile('response.txt')
+#      
+#     stashes = jPage["stashes"]
+#     stashes_count = len(stashes)
+#      
+#     empty_stashes_count = 0
+#     total_items_count = 0
+#     card_count = 0
+#      
+#     for s in range(stashes_count):
+#         items = stashes[s]["items"]
+#         if ( isEmpty(stashes[s]) ):
+#             empty_stashes_count += 1
+#         else:
+#             items = stashes[s]["items"]
+#             total_items_count += len(items)
+#             for i in range(len(items)):
+#                 if (getItemType(items[i]) == ITEM_TYPES.Card and getItemLeague(items[i]) == CURRENT_LEAGUE):
+#                     card_count += 1
+#                     item_name = getItemName(items[i])
+#                      
+#                     if isSellingBuyout(items[i]):
+#                         #print("{}: {} (~{}c)".format(item_name, ' '.join(getItemSellingOffer(items[i])[1:]), MARKET_PRICES[ITEM_TYPES.Card][item_name]))
+#                         print("{}: {} (~{}c)".format(item_name, ' '.join(getItemSellingOffer(items[i])[1:]), offer2chaos(getItemSellingOffer(items[i]))))                    
+#                         player_info = stashes[s]
+#                         player_info["items"] = []
+#                         dump_file.write("{} {}\n".format(player_info, items[i]))
+#      
+#     print("----")
+#     print("This page has {} items on {} stashes and {} empty stashes".format(total_items_count, stashes_count-empty_stashes_count, empty_stashes_count))
+#     print("{} divination cards on legacy league".format(card_count))
+#      
+#     next_page = jPage["next_change_id"]
+#     print("Next page: {}".format(next_page))
+#  
+# dump_file.close()
 
 print("Done!")
